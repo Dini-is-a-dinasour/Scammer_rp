dc76954ecafab2d1ac755d3f8579cb68fd2db04d
Dini-is-a-dinasour <DDeSilva@smail.pembinatrails.ca>
2026-04-15 11:32:44 -0500

    initial commit

---
+++ Menu.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+echo "hello there, this is a peak sim of a scammer to prepare you for future scammers. (this is just for fun)"
+sleep 2
+echo "do you want to continue? (choose the numbers from now on k?)"
+echo "1) Yes"
+echo "2) No"
+
+read choice
+
+case $choice in 
+ 1)
+ 	echo "nice, now I have to write the code twin."
+    ;;
+
+ 2)
+ 	echo "Okay, This entire code was meant to be about scam so this option simply is exit"
+    sleep 3
+ 	echo "womp womp"
+    ;;
+
+ *)
+ 	while true; do
+ 		echo "not one of the options buddy"
+ 	done
+    ;;
+
+esac
---
+++ Scammer1.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+for i in {1..3}
+do
+ sleep 1
+ echo "(scammer calling)"
+ echo "beep beep (ringtone)"
+done
+
+echo "(you pick up the phone)"
+echo "scammer: 'hello, how are you doing duday sir?"
+read $usless
+echo "well ids aboud do ged worse sir."
+sleep 1
+echo "Unfordunadly your microsofd accound has been compromised. Your accound Ireneedshelp@gmail.gom has been charged widh some very very naughty sduff sir."
+sleep 2
+
+echo "1) sir I dont have a microsoft account, Im a dedicated apple user bye."
+echo "2) Oh no! What ever could I do?"
+read $A 
+echo "you reply with '$A'"
+
+ case $A in 
+ 1)
+ 	echo "SHID"
+ 	sleep 1
+ 	echo "*line cuts twin*"
+ 	sleep 1
+ ;;
+
+2)
+	echo "Righd now dhe besd opdion is dto give me access do dhe compuder so dhad I can sdop dhe dhread."
+	echo "CODE INCOMPLETE HUZZ"
+
+
+ 
