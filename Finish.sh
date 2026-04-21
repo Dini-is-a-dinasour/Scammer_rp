aaf5a726f775856da169c133fb5e3622c5af210c
Dini-is-a-dinasour <DDeSilva@smail.pembinatrails.ca>
2026-04-21 11:50:36 -0500

    make sure to stop the code from going back to the previous file by typing exit 0 at the end

--- Finish.sh
+++ Finish.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-echo "hey there, its the system, you have succesfully beaten the scammer, very well done"
 sleep 2
+echo "hey there, its the system, you have succesfully beaten the scammer, very well done"
+sleep 3
 echo "in this constantly evolving capatalistic world we must prepare for people who use unethical means to gain profit."
-sleep 4
+sleep 5
 echo "This sim allows for users to get acustom to the modernizing scams, but its allways imperfect, so this project will be constantly modified to thwart scammers."
-sleep 6
+sleep 8
 echo "Did you really think I was actually serious"
 sleep 3
 echo "HA, I didnt mean any of that rubish, Ima just end it here cuz ur probably bored by now, dont get scammed homie."
 echo "oh and btw u did do the correct shi, nice work."
+exit 0
